0ac1c1e3f6689c485ae7f26f9df3b168d08e1a0d
Fernando Frascino <fernandofrascino@yahoo.com.br>
2020-03-28 18:24:33 -0300

    Complete mobile

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -13,7 +13,7 @@ export default function Detail() {
   const navigation = useNavigation();
 
   const incident = route.params.incident;
-  const message = `Olá ${incident.name}, entou entrando em contato pois gostaria de ajudar no caso ${incident.title} com o valor de ${Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}`;
+  const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso ${incident.title} com o valor de ${Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}`;
 
   function navigateBack() {
     navigation.goBack();
